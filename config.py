9cbef6e4d3ac4c74bae985791817990bec1feef7
David Njungo <davidnjungo3@gmail.com>
2021-08-23 05:36:40 +0300

    create configurations

---
+++ config.py
@@ -0,0 +1,32 @@
+import secrets
+import os
+
+class Base:
+    FLASK_APP = os.environ.get('FLASK_APP')
+    SQLACHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = secrets.token_hex()
+
+class Development(Base):
+    FLASK_ENV = os.environ.get("FLASK_ENV")
+    DATABASE = os.environ.get("DATABASE")
+    POSTGRES_USER = os.environ.get("POSTGRES_USER")
+    POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
+
+class Staging(Base):
+    DATABASE = ""
+    POSTGRES_USER = ""
+    POSTGRES_PASSWORD = ""
+    SQLALCHEMY_DATABASE_URI = ""
+
+class Production(Base):
+    DATABASE = ""
+    POSTGRES_USER = ""
+    POSTGRES_PASSWORD = ""
+    SQLALCHEMY_DATABASE_URI= ""
+   
+    DEBUG = True
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
